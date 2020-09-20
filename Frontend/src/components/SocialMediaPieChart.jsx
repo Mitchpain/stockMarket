05a6d61974c2f9ce7e9d00a61346da1acb923042
Mitchpain <michpoud@gmail.com>
2020-09-20 09:08:14 -0400

    added mouse interaction to the legend

--- Frontend/src/components/SocialMediaPieChart.jsx
+++ Frontend/src/components/SocialMediaPieChart.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import SocialMediaPieChartLegend from './SocialMediaPieChartLegend.jsx';
 import './styles/SocialMediaPieChart.scss';
 function SocialMediaPieChart(props) {
-    let displayLegend=false;
-    const conicString = props.socialMediaInfos.constructConicGradientString();
+    const [conicString] = React.useState(()=>{return props.socialMediaInfos.constructConicGradientString()});
+    const [displayLegend, setDisplayLegend] = React.useState(false);
     const divStyle = { 
         backgroundImage: 'conic-gradient(' + conicString + ')',
     };
+    const mouseEnter = ()=>{
+        setTimeout(function(){
+            setDisplayLegend(true);
+        }, 500);
+    }
     return (
         <div data-testid="socialMediaPieChart" 
         className="socialMedia-pieChart" style={divStyle}
-        onMouseEnter={()=>{displayLegend = true}}>
+        onMouseMove={()=>{setDisplayLegend(false);}}
+        onMouseEnter={()=>{mouseEnter();}}
+        onMouseLeave={()=>{setDisplayLegend(false);}}>
             {displayLegend &&
             <SocialMediaPieChartLegend socialMediaInfos={props.socialMediaInfos} />
             }
