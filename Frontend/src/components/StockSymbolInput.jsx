44febb24228629c358e81f8f22b21e2db6146b84
Mitchpain <michpoud@gmail.com>
2020-09-19 21:55:53 -0400

    Styling user input

--- Frontend/src/components/StockSymbolInput.jsx
+++ Frontend/src/components/StockSymbolInput.jsx
@@ -3,12 +3,15 @@ import StockInfo from "../app/StockInfo";
 
 function StockSymbolInput(props) {
   return (
-    <input type="text" placeholder="Stock symbol"
-      onChange={(e)=>{
-      if(StockInfo.validateStockSymbol(e.target.value)) {
-        props.onChangeStockSymbol(e.target.value)}
-      }
-    } />
+    <div className="inputContainer">
+      <input type="text" placeholder="Stock symbol"
+        onChange={(e)=>{
+        if(StockInfo.validateStockSymbol(e.target.value)) {
+          props.onChangeStockSymbol(e.target.value)}
+        }
+      } />
+      <span>Stock Symbol</span>
+    </div>
   );
 };
 
--- Frontend/src/components/TimeWindowInput.jsx
+++ Frontend/src/components/TimeWindowInput.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 function TimeWindowInput(props) {
 
   return (
-    <input type="number" min="1" placeholder="Time window" value={props.timeWindow}
-      onChange={(e)=>{
-        if(e.target.value > 0){
-          props.onChangeTimeWindow(e.target.value);
-        }}
-      }
-    />
+    <div className="inputContainer">
+      <input type="number" min="1" placeholder="Time window" value={props.timeWindow}
+        onChange={(e)=>{
+          if(e.target.value > 0){
+            props.onChangeTimeWindow(e.target.value);
+          }}
+        }
+      />
+      <span>Time Window (days)</span>
+    </div>
   );
 };
 
