b6f6ee90732fba01e1683ac61e0483f4d3ebcb43
Mitchpain <michpoud@gmail.com>
2020-09-20 09:49:08 -0400

    handle the mouse hover

--- Frontend/src/components/SocialMediaPieChart.jsx
+++ Frontend/src/components/SocialMediaPieChart.jsx
@@ -4,22 +4,36 @@ import './styles/SocialMediaPieChart.scss';
 function SocialMediaPieChart(props) {
     const [conicString] = React.useState(()=>{return props.socialMediaInfos.constructConicGradientString()});
     const [displayLegend, setDisplayLegend] = React.useState(false);
+    const [mousePosition, setMousePosition] = React.useState(()=>{return undefined});
+    const [mouseHasLeft, setMouseHasLeft] = React.useState(false);
     const divStyle = { 
         backgroundImage: 'conic-gradient(' + conicString + ')',
     };
-    const mouseEnter = ()=>{
-        setTimeout(function(){
-            setDisplayLegend(true);
-        }, 500);
-    }
+
+    React.useEffect(() => {
+        let timeoutID;
+        setDisplayLegend(false);
+        if(mousePosition != undefined && !mouseHasLeft){
+            timeoutID = window.setTimeout(() => {
+                setDisplayLegend(true);
+            }, 1000);  
+        }  
+        return () => {
+            if(timeoutID){
+                window.clearTimeout(timeoutID)
+            }
+        };
+    }, [mousePosition, mouseHasLeft]);
+
     return (
         <div data-testid="socialMediaPieChart" 
         className="socialMedia-pieChart" style={divStyle}
-        onMouseMove={()=>{setDisplayLegend(false);}}
-        onMouseEnter={()=>{mouseEnter();}}
-        onMouseLeave={()=>{setDisplayLegend(false);}}>
+        onMouseEnter={()=>{setMouseHasLeft(false);}}
+        onMouseMove={(e)=>{setMousePosition({x: e.clientX, y:e.clientY});}}
+        onMouseLeave={()=>{setMouseHasLeft(true);}}>
             {displayLegend &&
-            <SocialMediaPieChartLegend socialMediaInfos={props.socialMediaInfos} />
+            <SocialMediaPieChartLegend position={mousePosition}
+             socialMediaInfos={props.socialMediaInfos} />
             }
         </div> 
         
